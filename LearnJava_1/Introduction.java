e13bf54531160fbdf861955a5ab979e9f925453f
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-09-12 07:28:52 +0600

    first commit

---
+++ LearnJava_1/Introduction.java
@@ -0,0 +1,9 @@
+package LearnJava_1;
+
+public class Introduction {
+    //this method is true for all the program execution
+    public static void main(String args[]){
+        //prints
+        System.out.println("As salamua'likum");
+    }
+}
