5732523415bc618c5b7122f3f44ad3017765ca1f
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-09-19 16:09:04 +0600

    updated

--- learnJava_2/MainFunction.java
+++ learnJava_2/MainFunction.java
@@ -3,14 +3,17 @@ package learnJava_2;
 import jdk.nashorn.internal.parser.Scanner;
 
 public class MainFunction {
-    //Instances
-    Integer integerVar =new Integer();
-    Float floatVar = new Float();
-    S_tring stringVar =new S_tring();
-    Bool_ean boolVar =new Bool_ean();
-    //Actions
 
-    public static void main(String args[]){
 
+    public static void main(String[] args){
+        //Instances
+        Integer integerVar =new Integer();
+        Float floatVar = new Float();
+        S_tring stringVar =new S_tring();
+        Bool_ean boolVar =new Bool_ean();
+        //Actions
+        integerVar.addInt();
+        floatVar.addFloat();
+        
     }
 }
